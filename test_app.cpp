aa9ceb57b008a1c7f1db3de1becc1ce4b44658e5
Andrew Docking (Personal) <90356429+adocking@users.noreply.github.com>
2026-06-04 16:14:37 +0100

    First basic playback from SID.

--- test_app.cpp
+++ test_app.cpp
@@ -3,10 +3,11 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <cmath>
 #include <unistd.h>
-
 #include <sid.h>
 
 int tester();
+
+std::vector <short> _buffer;
 
 int main(int argc, char* argv[]) {
     std::cout << "reSID minimal CLI test app" << std::endl;
@@ -26,6 +27,8 @@ int main(int argc, char* argv[]) {
     printf( "Sid start\n" );
     std::cout << "SID: tone..." << std::endl;
 
+    int sampleRate = 44100;
+
     SID sid;
     sid.reset();
     sid.set_chip_model( MOS8580 );
@@ -36,6 +39,39 @@ int main(int argc, char* argv[]) {
         44100                   // audio sample rate
     );
 
+    // frequency (lower for audible envelope)
+    uint16_t freq = 6000;
+    sid.write(0x00, freq & 0xff);
+    sid.write(0x01, freq >> 8);
+
+    // ADSR (slow and obvious)
+    sid.write(0x05, 0xeA);
+    sid.write(0x06, 0xF0);
+
+    // gate ON (ONLY ONCE)
+    sid.write(0x04, 0x20 | 0x01);
+
+    // volume
+    sid.write(0x18, 0x0F);
+
+    // correct cycle math
+    int offset = 0;
+    int failsafe = 100000;
+    int seconds = 5;
+    int sampleCount = seconds * sampleRate;
+    _buffer.resize( sampleCount );
+
+    while ( offset < sampleCount && failsafe -- ) 
+    {
+        cycle_count clockDelta = (cycle_count)((985248.0 / 44100.0) * 512);
+        int out = sid.clock( clockDelta, _buffer.data() + offset, _buffer.size() - offset, 1 );
+        offset += out;
+
+        if ( out == 0 && clockDelta == 0 ) { break; }
+
+        printf( "offset: %i\n", offset );
+    }
+
     tester();
 
     return 0;
@@ -43,6 +79,7 @@ int main(int argc, char* argv[]) {
 
 static const double sampleRate = 44100.0;
 static double phase = 0.0;
+int _offset = 0;
 
 static OSStatus render(void *inRef,
                        AudioUnitRenderActionFlags *,
@@ -53,15 +90,12 @@ static OSStatus render(void *inRef,
 {
     float *buffer = (float *)ioData->mBuffers[0].mData;
 
-    double freq = 620.0;
-    double phaseInc = 2.0 * M_PI * freq / sampleRate;
-
     for (UInt32 i = 0; i < inNumberFrames; i++)
     {
-        buffer[i] = (float)sin(phase);
-        phase += phaseInc;
+        buffer[i] = _buffer[(_offset + i) % _buffer.size()] / 32768.f;
     }
 
+    _offset += inNumberFrames;
     return noErr;
 }
 
@@ -106,7 +140,7 @@ int tester()
     AudioUnitInitialize(unit);
     AudioOutputUnitStart(unit);
 
-    sleep(2);
+    sleep(5);
 
     AudioOutputUnitStop(unit);
     AudioUnitUninitialize(unit);
